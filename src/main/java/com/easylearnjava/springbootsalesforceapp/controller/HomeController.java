d37962eb20eebcda6b436a130f9da859ece9f5ad
Easy Learn Java <easylearnjava4u@gmail.com>
2017-10-22 03:03:37 -0500

    Create HomeController.java

---
+++ src/main/java/com/easylearnjava/springbootsalesforceapp/controller/HomeController.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		return "home";
+	}
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
