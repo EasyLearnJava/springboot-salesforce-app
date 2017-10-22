368aeaf88145c006a112ac1e89dc5409fc4ec007
Easy Learn Java <easylearnjava4u@gmail.com>
2017-10-22 02:47:11 -0500

    Add files via upload

---
+++ src/main/java/com/easylearnjava/springbootsalesforceapp/SpringbootSalesforceAppApplication.java
@@ -0,0 +1,12 @@
+package com.easylearnjava.springbootsalesforceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSalesforceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSalesforceAppApplication.class, args);
+	}
+}
